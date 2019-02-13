64e20a55fdb89dd3a2cdb120cdedc9e8576e07ee
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-02-13 20:40:58 +0700

    Add files via upload

---
+++ precache-manifest.bf0d5cd333da6b2b97bf27d62534f210.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f1ae8ad370affddba8e",
+    "url": "/Portfolio/static/css/main.ddbcab28.chunk.css"
+  },
+  {
+    "revision": "6f1ae8ad370affddba8e",
+    "url": "/Portfolio/static/js/main.6f1ae8ad.chunk.js"
+  },
+  {
+    "revision": "3806ffb9cd92b0f8ff82",
+    "url": "/Portfolio/static/js/1.3806ffb9.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "898271715037236695928d628b68450c",
+    "url": "/Portfolio/static/media/preview-256.89827171.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d14e4fd3c8b856f05612643d6773e497",
+    "url": "/Portfolio/static/media/react.d14e4fd3.png"
+  },
+  {
+    "revision": "c3415a592eccdb63bb5242a023fdcfad",
+    "url": "/Portfolio/static/media/react-redux.c3415a59.png"
+  },
+  {
+    "revision": "83e667b75d5c02ffba2765b0a2f927b7",
+    "url": "/Portfolio/static/media/router.83e667b7.png"
+  },
+  {
+    "revision": "ed344d100f9226f12c81f38510c3742c",
+    "url": "/Portfolio/static/media/redux.ed344d10.png"
+  },
+  {
+    "revision": "466115f73dc93d1d99f4237f76ffe620",
+    "url": "/Portfolio/static/media/redux-saga.466115f7.svg"
+  },
+  {
+    "revision": "acd5e3c58877bf83db529784755c0f63",
+    "url": "/Portfolio/static/media/form.acd5e3c5.png"
+  },
+  {
+    "revision": "f476a70ec454d7564cb200c627037deb",
+    "url": "/Portfolio/static/media/bootstrap-stack.f476a70e.png"
+  },
+  {
+    "revision": "9edcb5106c553737a172ec833810fe2e",
+    "url": "/Portfolio/static/media/jest-logo.9edcb510.png"
+  },
+  {
+    "revision": "e051f1a2711a770c526c1d48768c606a",
+    "url": "/Portfolio/static/media/webpack-icon.e051f1a2.png"
+  },
+  {
+    "revision": "2022e37e4154d43f400564a7a6f8dec7",
+    "url": "/Portfolio/static/media/restapi.2022e37e.png"
+  },
+  {
+    "revision": "f8dab57d048fabd69ea16c67e1615b86",
+    "url": "/Portfolio/static/media/express.js.f8dab57d.png"
+  },
+  {
+    "revision": "1637e8ea012d88a1c0cbb4cc4e29bb08",
+    "url": "/Portfolio/static/media/webstorm_logo.1637e8ea.png"
+  },
+  {
+    "revision": "9e345731f7705c98a07845343115e32f",
+    "url": "/Portfolio/index.html"
+  }
+];
